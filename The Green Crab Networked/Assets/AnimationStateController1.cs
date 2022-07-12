68fdf8ee217e5d97c9fa9f93ef6c3608606a44ad
soloroyce <71810492+soloroyce@users.noreply.github.com>
2022-07-12 23:14:38 +0800

    Added LeeKuanYew Animation Controller

---
+++ The Green Crab Networked/Assets/AnimationStateController1.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateController1 : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        Debug.Log(animator);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isWalking = animator.GetBool("isWalking");
+        bool forwardPressed = Input.GetKey("w");
+        bool isSpinning = Input.GetKey("a");
+    
+        if (!isWalking && forwardPressed)
+        {
+            animator.SetBool("isWalking", true);
+        }
+
+        if (isWalking && !forwardPressed)
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        if (forwardPressed && isSpinning)
+        {
+            animator.SetBool("isSpinning", true);
+         
+        }
+        if(!forwardPressed || !isSpinning)
+        {
+            animator.SetBool("isSpinning", false);
+        }
+    }
+}
+
+
