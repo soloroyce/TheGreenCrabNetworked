037dd3dc3b070b20f3a649b80f3feac53007cd6f
Longman <info@longman.hk>
2022-09-21 05:22:51 +0800

    organizing
    aligned names of gameobjects, audio files, and descriptions

--- The Green Crab Networked/Assets/Audio Scripts/voArray.cs
+++ The Green Crab Networked/Assets/Audio Scripts/voArray.cs
@@ -1,24 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class VOarray : MonoBehaviour
-{
-    public AudioSource _as;
+// public class VOarray : MonoBehaviour
+// {
+//     public AudioSource _as;
     
-    public AudioClip[] audioClipArray;
-    // Start is called before the first frame update
-    void void Awake() {
-        _as = GetComponent<AudioSource>();
-    }
-    void Start()
-    {
-        _as.clip=audioClipArray[]
-    }
+//     public AudioClip[] audioClipArray;
+//     // Start is called before the first frame update
+//     void void Awake() {
+//         _as = GetComponent<AudioSource>();
+//     }
+//     void Start()
+//     {
+//         _as.clip=audioClipArray[]
+//     }
 
-    // Update is called once per frame
-    void Update()
-    {
+//     // Update is called once per frame
+//     void Update()
+//     {
         
-    }
-}
+//     }
+// }
